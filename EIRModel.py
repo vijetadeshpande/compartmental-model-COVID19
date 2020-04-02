aecf9e09a76cdba1d1b449cb6b8b967808204d19
Vijeta <vkdeshpande@umass.edu>
2020-04-02 02:44:21 -0400

    started writing structure for optimization module required for parameterization

--- EIRModel.py
+++ EIRModel.py
@@ -11,91 +11,110 @@ import numpy as np
 
 class EIRModel:
     def __init__(self, initial_pop, rate_matrix_file_path):
-        rate_matrix = pd.read_excel(rate_matrix_file_path, header = 0, index_col = 0).fillna(0)
-        self.q = rate_matrix.values
-        self.compartments = rate_matrix.index.values
+        
+        # simple check
         if len(initial_pop) == len(self.compartments):
             self.init_pop = initial_pop
         else:
             print('Length of initial population should be equal to number of compartments')
+        
+        # set main attributes
+        rate_matrix = pd.read_excel(rate_matrix_file_path, header = 0, index_col = 0).fillna(0)
+        self.q = rate_matrix.values
+        self.compartments = rate_matrix.index.values
+        self.compartment_index = {}
+        for com in self.compartments:
+            self.compartment_index[com] = np.where(self.compartments == com)
+        
+        # few hyper-par
         self.unit_time = np.divide(1, 1)
         self.reproduction_rate = {'no social distancing': 3.88/11.5, 
                                   'social distancing': 1.26/11.5,
                                   'quarantining': 0.32/11.5}
-        self.births = []
-        self.recoveries = []
-        self.deaths = []
-        self.population_t = []
-        self.entering_population = None  
-        self.incidence = None
-        self.new_hospitalization = None
+        
+        # few important estimates which might be required for each time step
+        self.epi_markers = {'diagnosed new': [],
+                            'diagnosed cumulative': initial_pop[3],
+                            'births': [],
+                            'recoveries': [],
+                            'deaths': [],
+                            'population instantaneous': None,
+                            'hospitalizations': []
+                }
+        self.insta_population = 0
+        self.delta_population = 0
+        
+        # what are we calibrating
+        decision_mat = np.zeros((len(self.compartments), len(self.compartments)))
+        decision_mat[1:3][0] = 1
+        self.decision_var = decision_mat
+    
+    def extract_val(self, com):
+        delta_pop = self.delta_population
+        idx = self.compartment_index[com]
+        
+        return delta_pop[idx]
+    
+    def update_epi_markers(self):
+        
+        # extract delta_pop
+        delta_pop = self.delta_population
+        
+        # update diagnosed cases
+        x = self.extract_val('diagnosed')
+        self.epi_markers['dignosed new'].append(x)
+        self.epi_markers['diagnosed cimulative'] += x
+        
+        # update births, i.e. new transmissions
+        x = self.extract_val('births')
+        self.epi_markers['births'].append(x)
+        
+        # update recoveries
+        x = self.extract_val('recovery')
+        self.epi_markers['recoveries'].append(x)
+        
+        # update deaths
+        x = self.extract_val('death')
+        self.epi_markers['deaths'].append(x)
+        
+        # hospitalizations
+        x = self.extract_val('severe cases') + self.extract_val('non-severe cases')
+        self.epi_markers['hospitalizations'].append(x)
+        
+        return
     
     def calculate_pop_change(self, pop_cur):
         delta_pop = np.multiply(np.multiply(np.reshape(pop_cur, (len(pop_cur),1)), self.q), self.unit_time)
-        
         #print(('Following number should be close to zero: %f')%(sum(sum(delta_pop))*1000000000))
         
-        self.entering_pop = delta_pop
+        # set attribute
+        self.delta_population = delta_pop
         
-        return delta_pop
+        return delta_pop    
     
-    def calculate_new_infections(self):
-        # take new cases from exposed, symptomatic and diagnosed
-        self.incidence = np.multiply(self.entering_population, np.array([1,1,1,0,0,0,0]))
-        
-        return
     
-    def calculate_new_hospitalization(self):
-        # take new hospitalizations from severe and non-severe compartments
-        self.new_hospitalization = np.multiply(self.entering_population, np.array([0,0,0,1,1,0,0]))
-        
-        return
-    
-    def calculate_births(self, pop_prev, social_distancing = 'no'):
-        
-        # get rep. num
-        if social_distancing == 'no':
-            r_num = self.reproduction_rate['no social distancing']
-        elif social_distancing == 'yes':
-            r_num = self.reproduction_rate['social distancing']
-        else:
-            r_num = self.reproduction_rate['quarantining']
-        
-        # multiply exposed and undiagnosed cases
-        births = np.multiply(np.sum(np.multiply(r_num, np.multiply(pop_prev, [1, 1, 0, 0, 0, 0, 0]))), self.unit_time)
-        
-        return births
-     
-    def calculate_recoveries(self, pop_prev):
-        recoveries = np.sum(np.multiply(pop_prev, [0, 0, 0, 0, 0, 1, 0]))
-        
-        return recoveries
-    
-        
     def forward(self, pop_prev, social_distancing = 'no'):
         
         # store population
-        self.population_t.append(pop_prev)
-        births = self.calculate_births(pop_prev, social_distancing)
-        self.births.append(births)
-        recovs = self.calculate_recoveries(pop_prev)
-        self.recoveries.append(recovs)
+        self.insta_population = pop_prev
         
         # calculate change in pop size
         delta_pop = self.calculate_pop_change(pop_prev)
          
         # update population
         pop_cur = pop_prev + np.sum(delta_pop, axis = 0)
-         
-        # compute new indections and new hospitalizations
-        #self.calculate_new_infections()
-        #self.calculate_new_hospitalization()
-         
-        # calculate birth
-        pop_cur[0] += births
+        
+        # cupdate epi markers
+        self.update_epi_markers()
          
         return pop_cur
-     
-
     
+    def backward(self):
+        
+        # extract the model output to which we are fitting the model
+        # number of diagnosed cases
+        
+        return
+        
+        
 
---
+++ OptiAlgo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr  2 02:35:20 2020
+
+@author: vijetadeshpande
+"""
+
+class GD:
+    def error_(self, y_hat, y):
+        
+        
+        return
+    
+    def update_(self, par, error, step_size):
+        
+        
+        return
