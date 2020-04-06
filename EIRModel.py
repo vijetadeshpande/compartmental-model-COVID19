c2d469748da8c23363ab6fa469594d64346c1a4d
Vijeta <vkdeshpande@umass.edu>
2020-04-05 21:22:48 -0400

    Finished curve fitting module for estimating the diagnosis rate of symptomatic cases. Stll need to make adjustments for asymptomatic cases.

--- EIRModel.py
+++ EIRModel.py
@@ -15,7 +15,9 @@ class EIRModel:
     def __init__(self, initial_pop, rate_matrix_file_path, divide_time_step = 3600):
         
         # set main attributes
-        self.init_pop = initial_pop
+        self.day_count = 0
+        self.pop_init = initial_pop
+        self.pop_day_start = initial_pop
         rate_matrix = pd.read_excel(rate_matrix_file_path, header = 0, index_col = 0).fillna(0)
         self.q = rate_matrix.values
         self.compartments = rate_matrix.index.values
@@ -36,9 +38,10 @@ class EIRModel:
                             'recoveries': [],
                             'deaths': [],
                             'population instantaneous': None,
-                            'hospitalizations': []
+                            'hospitalizations': [],
+                            'testing rate': []
                 }
-        self.instantaneous_pop = 0
+        self.pop_day_end = 0
         self.delta_population = 0
         
         # what are we calibrating
@@ -95,7 +98,7 @@ class EIRModel:
         
         for t in range(0, int(1/self.unit_time)):
             # store population
-            self.instantaneous_pop = pop_prev
+            #self.instantaneous_pop = pop_prev
             
             # calculate change in pop size
             delta_pop = self.calculate_pop_change(pop_prev)
@@ -111,37 +114,86 @@ class EIRModel:
              
         return pop_cur
     
-    def calculate_error(self, desc_var, day):
+    def calculate_error(self, desc_var):
+        # extract values
+        day = self.day_count
         
         # first do forward pass
-        pop_prev = self.instantaneous_pop
+        pop_prev = self.pop_day_start
         pop_est = self.forward(pop_prev)
         
         # extract data to match to
         pop_act = self.calibration_data[day]
         
         # calculate squared error
-        err = mean_squared_error(pop_act, pop_est[self.compartment_idx['diagnosed']])
+        err = mean_squared_error([pop_act], [pop_est[self.compartment_index['diagnosed']]])
         
         return err
+    
+    def update_rate_par(self, rate_mat, s1, s2, val):
+        
+        # get index of states
+        s1_idx, s2_idx = self.compartment_index[s1], self.compartment_index[s2]
+        
+        # update rate parameter
+        rate_mat[s1_idx][s2_idx] = val
+        
+        # update diagonal element
+        rate_mat[s1_idx][s1_idx] = -1 * np.sum(rate_mat[s1_idx][s2_idx+1:])
+        
+        return rate_mat
+        
+    def forward_fit(self, x_data, d_rate):
+        
+        # here x_data is day
+        # y_datai is diagnosed cases
+        # p0 is diagnosis rate (testing rate)
+        
+        # pop at day start (this will be used as initial population for this day)
+        pop_start = self.pop_day_start
+        
+        # to have population at day end, we need a forward pass with par p0 in the rate matrix
+        rate_mat = self.q
+        
+        # update value of diagnosis/testinf rate
+        rate_mat = self.update_rate_par(rate_mat, 'symptomatic', 'diagnosed', d_rate)
+        self.q = rate_mat
+        
+        # forward pass
+        pop_end = self.forward(pop_start)
+        self.pop_day_end = pop_end
+        
+        # predicted value of y_hat: predicted value of diagnoded cases
+        y_hat = np.array([pop_end[self.compartment_index['diagnosed']]])
+        
+        return y_hat
         
     
     def backward(self, day):
         
         # what's decision variable?, initialize it
         desc_var_init = []
-        desc_var_init.append(self.q[self.compartment_index['exposed']][self.compartment_index['births']])
-        desc_var_init.append(self.q[self.compartment_index['symptomatic']][self.compartment_index['births']])
+        #desc_var_init.append(self.q[self.compartment_index['exposed']][self.compartment_index['births']])
+        desc_var_init.append(self.q[self.compartment_index['symptomatic']][self.compartment_index['diagnosed']])
+        #desc_var_init.append(day)
         
-        # optimize, this is fitted solution
-        desc_var = opt.least_squares(fun = self.calculate_error, 
-                                     x0 = np.array([desc_var_init, day]))
+        # scipy curve fit
+        d_rate = opt.curve_fit(f = self.forward_fit, 
+                               xdata = np.array(day),
+                               ydata = np.array(self.calibration_data[day]),
+                               p0 = desc_var_init,
+                               method = 'lm')
+        
+        # store testing rate for the day
+        self.epi_markers['testing rate'].append(d_rate[0])
+        
+        # print
+        print(('Testing rate on day %f is: %f')%(day, d_rate[0][0]))
         
         # change rate matrix
-        q = self.q
-        q[self.compartment_idx['asymptomatic']][self.compartment_idx['births']] = desc_var[0]
-        q[self.compartment_idx['symptomatic']][self.compartment_idx['births']] = desc_var[1]
-        self.q = q
+        rate_mat = self.q
+        rate_mat = self.update_rate_par(rate_mat, 'symptomatic', 'diagnosed', d_rate[0][0])
+        self.q = rate_mat
         
         return
         
--- main_EIR.py
+++ main_EIR.py
@@ -13,19 +13,29 @@ import numpy as np
 file_path = r'/Users/vijetadeshpande/Documents/GitHub/compartmental-model-COVID19/rate_matrix.xlsx'
 
 # initialize model
-covid = EIRModel.EIRModel(initial_pop = np.array([0, 40, 10, 0, 0, 0, 0, 0]), 
+covid = EIRModel.EIRModel(initial_pop = np.array([0, 0, 4, 1, 0, 0, 0, 0]), 
                           rate_matrix_file_path = file_path)
 
 # loop over some horizon
-pop_prev = covid.init_pop
+pop_prev = covid.pop_day_start
 social_dist = 'no'
 
-for day in range(0, 40):
+for day in range(0, 8):
+    # update population at start of day and day 
+    covid.day_count = day
+    covid.pop_day_start = pop_prev
+    
+    # some assumptions
     if (day == 21 or day == 35):
         social_dist = ('yes' * int(day == 14)) + ('q' * int(day == 30))
+    
     # forward pass
     pop_cur = covid.forward(pop_prev, social_dist)
     pop_prev = pop_cur
+    
+    # update instantaneous pop (this can also be used a initial population for the next day)
+    covid.pop_day_end = pop_prev
+    
     # backward pass
     covid.backward(day)
 
